cf77713f31ac7af90bfbb54f9d956e9fde758e41
margarcuae <margar.tcuae@gmail.com>
2017-10-15 16:27:29 -0500

    llenado prueba

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,61 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        
+        for ($i=0; $i <5 ; $i++) { 
+	        DB::table('users')->insert([
+	            'email' => str_random(10).'@gmail.com',
+	            'password' => bcrypt('secret'),
+	            'nombre' => 'nombre'.i,
+	            'apellido' => 'apellido'.i,
+	            'direccion' => 'direccion'.str_random(10),
+	            'telefono' =>'telefono'.str_random(10),
+	            'monto' => 9898,
+	            'foto' => 'foto'.i,
+	        ]);
+        }
+
+        for ($i=0; $i <3 ; $i++) { 
+        	
+        	DB:table('trabajadors')->insert([
+        		'user_id' => i,
+        		'dni' => 27876543,
+        		'descripcion' => str_random(20),
+        		]);
+        }
+
+        for ($i=0; $i <20 ; $i++) { 
+        	DB:table('tags')([
+        		'nombre' => 'tag'.i,
+
+        		]);
+        }
+
+        for ($i=0; $i <3 ; $i++) { 
+        	DB:table('trabajos')->insert([
+        		'nombre'=> 'trabajo'.i,
+        		'ubicacion' => 'ubicacion'.i,
+        		'descripcion'=> 'descripcion'.i,
+        		'trabajador_id'=> i,
+
+        		]);
+        }
+
+        for ($i=0; $i <5 ; $i++) { 
+        	DB:table('calificacions')->insert([
+        		'user_id' => i,
+        		'trabajo_id' => 1,
+        		'calificacion' => i,
+        		]);
+        }
+        for ($i=0; $i <3 ; $i++) { 
+        	DB:table('fotos')->insert([
+        		'url' => 'url'.i.'.jpg',
+        		'trabajo_id' => i,
+        		]);
+        }
+        
+
+
     }
 }
