a2c39cc5da07fb218219ca548174fb8f3bae3ddf
luisbch9 <luisbch9@gmail.com>
2017-10-15 17:31:45 -0500

    Navbar (Bulma)

--- public/jsa/main.js
+++ public/jsa/main.js
@@ -6,24 +6,13 @@ Vue.component('barra-busqueda',{
 	},
 
 	template: `
-		<input id="busqueda" class="col s6  green lighten-1 valign-wrapper" type="text" v-model="texto">
-		</input>
+	
 	`
 });
 
 Vue.component('barra-navegacion', {
   template: `
-    <div class="nav-wrapper row green accent-4">
-      <div class="brand-logo" style="padding-left:20px"> LoJobs </div>
-      <div class="col s3 offset-s2"> <barra-busqueda> </barra-busqueda> </div>
-
-        <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">JavaScript</a></li>
-      </ul>
-
-    </div> 
+  
   `
 });
 
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,45 +1,95 @@
 <!DOCTYPE html>
 <html>
 <head>
-	  <!--Import Google Icon Font-->
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <!--Import materialize.css-->
-      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-      <!--Let browser know website is optimized for mobile-->
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="css/bulma.css">
+
 	<title>@yield('titulo')</title>
 
-	<style type="text/css">
-		#busqueda{
-    		margin-top: 15px;
-		    height: 30px;
-
-		}
-
-		input{
-			-webkit-border-radius: 50px;
-			-moz-border-radius: 50px;
-			border-radius: 50px;
-		}
-		
-	</style>
 </head>
 <body>
 
+	<nav class="navbar is-light" >
+		<div class="navbar-brand ">
+			<div class="navbar-item">
+					<img src="img/logo.png" style="max-height: 2.5rem">
+			</div>
+  		</div>
+
+  		<div class="navbar-menu">
+  			<div class="navbar-start">
+  				<div class="navbar-item">
+  					<div class="control has-icons-left">
+	  					<input class="input is-medium" type="text" placeholder="Buscar trabajos">
+	  					<span class="icon is-small is-left">
+						    <i class="fa fa-suitcase" aria-hidden="true"></i>
+	    				</span>
+	  				</div>
+  					
+  				</div>
+  			</div>
+
+
+  			<div class="navbar-end">
+
+  				<a class="navbar-item">
+  					Publicar Trabajo
+  				</a>
+
+  				<a href="" class="navbar-item">
+  					Registrarse
+  				</a>
+  				
+  				<a href="" class="navbar-item">
+  					Iniciar Sesión
+  				</a>
+
+  				<!--
+  				<div class="navbar-item has-dropdown">
+					<a class="navbar-link">
+						Iniciar Sesión
+					</a>
+
+					<div class="navbar-dropdown">
+						<a class="navbar-item">
+							Overview
+						</a>
+					</div>
+				</div>
+				-->
+
+  			</div>
+  		</div>
+
+  		
+	</nav>
+
+	<div class="container" >
+
+		<section class="hero">
+			<div class="hero-body">
+				<div class="container">
+					<h1 class="title">Recomendados para ti</h1>
+					<h2 class="subtitle">Creemos que los podrías necesitar</h2>
+				</div>
+			</div>
+		</section>
+
+	</div>
+
 
 	<div id="aplicacion">
-	<nav>
-		<barra-navegacion>
-	</nav>
+	
    		@yield('body')
 
 	</div>
    
 
-  <!--Import jQuery before materialize.js-->
-  <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
-  <script type="text/javascript" src="js/materialize.min.js"></script>
+
 
   <script type="text/javascript" src="js/vue.js"></script>
 
