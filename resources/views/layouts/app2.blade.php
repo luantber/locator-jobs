e591326ad56955ae2452ba3629b4e6d79b5de210
alrus2797 <alrus2797@gmail.com>
2017-11-20 18:07:30 -0500

    app2 fix

--- resources/views/layouts/app2.blade.php
+++ resources/views/layouts/app2.blade.php
@@ -3,10 +3,14 @@
 <head>
 	<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script src="js/jquery-3.2.1.min.js"></script>
-    <link rel="stylesheet" href="css/fonts-awesome.min.css">
 
-    <link rel="stylesheet" href="css/bulma.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="{{asset('js/jquery-3.2.1.min.js')}}"></script>
+    <link rel="stylesheet" href="{{asset('css/fonts-awesome.min.css')}}">
+
+    <link rel="stylesheet" href="{{asset('css/bulma.css')}}">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
 	<title>@yield('titulo')</title>
 
