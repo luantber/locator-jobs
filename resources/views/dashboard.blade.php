152a45cbded410b894fb7a06401455f72da79486
luisbch9 <luisbch9@gmail.com>
2017-12-08 23:53:36 -0500

    v0.3

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -102,10 +102,75 @@
       </div> 
     </div> 
  
-    <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">  <div class="container-fluid tp-3"> 
-        WIpooooow x2 
+    <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab"> 
+       <div class="container-fluid mt-2"> 
+
+       <strong> Solicitudes: </strong>
+
+       	<div class="row">
+        	<div class="col-1">
+        		<img src="img/ernesto.jpg" alt="..." class="rounded-circle" style="width: 70px; height: 65px;">
+        	</div>
+        	<div class="col-8">
+        		<strong>Luis Antonio Bernal</strong><br>
+        		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        		consequat. 
+        	</div>
+
+        	<div class="col pt-4"> Aceptar </div>
+        	<div class="col pt-4"> Rechazar </div>
+        	<div class="col pt-4"> <a href="">Mensaje</a> </div>
+        </div>
+
+        <div class="row">
+        	<div class="col-1">
+        		<img src="img/ernesto.jpg" alt="..." class="rounded-circle" style="width: 70px; height: 65px;">
+        	</div>
+        	<div class="col-8">
+        		<strong>Luis Antonio Bernal</strong><br>
+        		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        		consequat. 
+        	</div>
+
+        	<div class="col pt-4"> Aceptar </div>
+        	<div class="col pt-4"> Rechazar </div>
+        	<div class="col pt-4"> <a href="">Mensaje</a> </div>
+        </div>
+
+       	<strong> Comentarios: </strong>
+
+        <div class="row">
+        	<div class="col-1">
+        		<img src="img/ernesto.jpg" alt="..." class="rounded-circle" style="width: 70px; height: 65px;">
+        	</div>
+        	<div class="col">
+        		<strong>Luis Antonio Bernal</strong><br>
+        		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        		consequat. 
+        	</div>
+        </div>
+
+        <div class="row">
+        	<div class="col-1">
+        		<img src="img/ernesto.jpg" alt="..." class="rounded-circle" style="width: 70px; height: 65px;">
+        	</div>
+        	<div class="col">
+        		<strong>Juanito Perez Jarra</strong><br>
+        		Duis aute irure dolor in reprehenderit in voluptate velit esse
+        		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        	</div>
+        </div>
+
+
       </div> 
-    </div> 
+    </div>  
    
   </div> 
    
